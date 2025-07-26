0f61088afeaaff4a9a07ddd13066ea482e2f9edb
2300031594pranay <s.pranaybalaji888@gmail.com>
2025-07-26 16:05:01 +0530

    deleted one line from footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,6 @@ export default function Footer() {
     <>
       <footer className="footer">
         <div className="footer-container">
-          <p>Developed by Pranay Balaji</p>
           <p>&copy; 2025 PCAP Practice Platform. All rights reserved.</p>
           <p>Practice makes perfect! Good luck with your PCAP certification.</p>
           <a
